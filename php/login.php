0f54100f4fb3f470efde9a0967afc731ba9c6d0e
Enrique Robles <roblessalazarjorgeenrique@gmail.com>
2022-09-13 18:28:21 -0500

    Login exitoso por roles

--- php/login.php
+++ php/login.php
@@ -1,32 +1,29 @@
-
 <?php
 
- include('conexion.php');
- session_start();
+include('conexion.php');
+session_start();
+
 $usu = $_POST["email"];
 $pass = $_POST["password"];
 $rol = $_POST["rol"];
 
-
-
-$queryusuario = pg_query($connection,"SELECT * FROM public.roles WHERE usuario ='$usu' and pass = '$pass' and rol = '$rol'");
-$nr 		= pg_num_rows($queryusuario);  
-	
-if ($nr == 1 )  
-	{ 
-		if($rol=="Profesor")
-			{	
-				header("Location: pag_alumno.php");
-			}
-		else if ($rol=="Alumno")
-			{
-				header("Location: pag_profesor.php");
-			}
-	}
-else
-	{
-	echo "<script> alert('Usuario, contraseña o rol incorrecto.');window.location= '../index.html' </script>";
-	}
-
-	pg_close()
+if($rol=="Profesor"){	
+		$queryusuario2 = pg_query($connection,"SELECT * FROM public.profesor WHERE email ='$usu' and contrasena = '$pass' and rol = '$rol'");
+		$nr2 = pg_num_rows($queryusuario2);  
+		if ($nr2 == 1 ) {
+			header("Location: pag_profesor.php");
+		}else{
+			echo "<script> alert('Usuario, contraseña o rol incorrecto.');window.location= '../index.html' </script>";
+			pg_close();
+		}	
+}else if ($rol=="Alumno"){
+	$queryusuario = pg_query($connection,"SELECT * FROM public.alumno WHERE email ='$usu' and contrasena = '$pass' and rol = '$rol'");
+	$nr = pg_num_rows($queryusuario);  
+		if ($nr == 1) {
+			header("Location: pag_alumno.php");
+		}else{
+			echo "<script> alert('Usuario, contraseña o rol incorrecto.');window.location= '../index.html' </script>";
+			pg_close();
+		}
+}
 ?>
--- php/pag_alumno.php
+++ php/pag_alumno.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Plataforma Unam</title>
-    <link href="CSS/bootstrap.min.css" rel="stylesheet">
-    <link href="CSS/myEstilo.css" rel="stylesheet">
+    <link href="../CSS/bootstrap.min.css" rel="stylesheet">
+    <link href="../CSS/myEstilo.css" rel="stylesheet">
 
 </head>
 <body>
