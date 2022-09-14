756c4cb87f4bb2bf9a7959eba83204b4e217101c
Enrique Robles <roblessalazarjorgeenrique@gmail.com>
2022-09-13 19:15:37 -0500

    Cierre de sesion implementado

--- php/login.php
+++ php/login.php
@@ -11,6 +11,7 @@ if($rol=="Profesor"){
 		$queryusuario2 = pg_query($connection,"SELECT * FROM public.profesor WHERE email ='$usu' and contrasena = '$pass' and rol = '$rol'");
 		$nr2 = pg_num_rows($queryusuario2);  
 		if ($nr2 == 1 ) {
+			$_SESSION['nombredelusuario']=$usu;
 			header("Location: pag_profesor.php");
 		}else{
 			echo "<script> alert('Usuario, contraseña o rol incorrecto.');window.location= '../index.html' </script>";
@@ -20,6 +21,7 @@ if($rol=="Profesor"){
 	$queryusuario = pg_query($connection,"SELECT * FROM public.alumno WHERE email ='$usu' and contrasena = '$pass' and rol = '$rol'");
 	$nr = pg_num_rows($queryusuario);  
 		if ($nr == 1) {
+			$_SESSION['nombredelusuario']=$usu;
 			header("Location: pag_alumno.php");
 		}else{
 			echo "<script> alert('Usuario, contraseña o rol incorrecto.');window.location= '../index.html' </script>";
--- php/pag_profesor.php
+++ php/pag_profesor.php
@@ -1,52 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Plataforma Unam</title>
-    <link href="../CSS/bootstrap.min.css" rel="stylesheet">
-    <link href="../CSS/myEstilo.css" rel="stylesheet">
+<title>Plataforma Unam</title>
+<link href="../CSS/bootstrap.min.css" rel="stylesheet">
+<link href="../CSS/myEstilo.css" rel="stylesheet">
+<link href="../CSS/myEstilo2.css" rel="stylesheet">
+<link rel="stylesheet" href="login.css">
 
-</head>
-<body>
-    <!--Se coloca imagen y formulario-->
-    <div class="container mt-5">
-        <!--fila-->
-        <div class="row align-items-stretch">
-            <!--columna-->
-            <!--Se hace el breakpoint de la imagen-->
-            <div class="col bg d-none d-lg-block"></div>
-            <div class="col">
-                <h2 class="fw-bold text-center py-5">Bienvenido Profesor</h2>
-                <!--Formulario login-->
-                <form action="php/loginProfesor.php" method="post" action>
-                    <!--Campos a Rellenar-->
-                    <div class="mb-4">
-                        <label for="email" class="form-label">Correo</label>
-                        <input type="email" class="form-control" name="email" required> 
-                    </div>
-                    <div class="mb-4">
-                        <label for="password" class="form-label">Contraseña</label>
-                        <input type="password" class="form-control" name="password" maxlength="8" minlength="2" required> 
-                    </div>
+<table>
+<?php
+include('conexion.php');
+session_start();
 
-                    <!--Boton-->
-                    <div class="d-grid">
-                        <button type="submit" class="btn btn-primary" name="login" value="login" >Iniciar Sesión</button>
-                    </div>
+if(isset($_SESSION['nombredelusuario']))
+{
+	$usuarioingresado = $_SESSION['nombredelusuario'];
+	echo "<tr><td colspan='2' align='center'><h1>Bienvenido: $usuarioingresado </h1></td></tr>";
+}
+else
+{
+	header('location: ../index.html');
+}
+?>
 
-                    <!--Link Registro-->
-                    <div class="my-3">
-                        <span>No tienes cuenta? <a class="a" href="registroProfesores.html">Registro Profesor</a> </span> 
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
+<!--Boton para cerrar sesion-->
+<form method="POST">
+<tr><td colspan='2' align="center" ><input type="submit" value="Cerrar sesión" name="btncerrar" /></td></tr>
+</form>
 
-
+<!--Código para cerrar sesion-->
+<?php 
+if(isset($_POST['btncerrar']))
+{
+	session_destroy();
+	header('location: ../index.html');
+}
+?>
     <script src="JS/bootstrap.bundle.min.js" ></script>
     <script src="JS/bootstrap.min.js" ></script>
-</body>
-</html>
+</table>
